ed068e7a94b026d69bb6222125c7aeaeffa546c8
Bastien Saidi <saidi.ireland@gmail.com>
2021-04-28 16:24:44 +0100

    Experimental color for errors.

--- main.py
+++ main.py
@@ -4,11 +4,42 @@ from flask import request, jsonify, render_template, Response
 from lxml.html import parse
 from bs4 import BeautifulSoup
 
+
+class CustomFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors"""
+
+    grey = "\x1b[38;21m"
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 app = flask.Flask(__name__, static_url_path='')
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(CustomFormatter())
+log.addHandler(ch)
+
 app.config["DEBUG"] = True
 threads = {}
+
+
 
 def getMime(url):
 	if url.endswith('css'):
